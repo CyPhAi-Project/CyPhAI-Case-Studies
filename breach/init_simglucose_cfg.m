3e2645f388dd774db74e421531d5aa900aa3b15e
Alexandre Donze <alex@decyphir.com>
2024-09-06 10:59:29 +0200

    added first file for breach interface

---
+++ breach/init_simglucose_cfg.m
@@ -0,0 +1,38 @@
+cfg = struct();
+cfg.patient.range = [0,29];
+cfg.patient.value = 1;
+cfg.meal.breakfast_time.value = 8;
+cfg.meal.breakfast_time.range = [5, 9];
+cfg.meal.breakfast_size.value = 50;
+cfg.meal.breakfast_size.range = [40, 60];
+
+cfg.meal.snack1_time.value = 10;
+cfg.meal.snack1_time.range = [9, 11];
+cfg.meal.snack1_size.value = 20;
+cfg.meal.snack1_size.range = [10, 30];
+
+cfg.meal.lunch_time.value = 12;
+cfg.meal.lunch_time.range = [11, 13];
+cfg.meal.lunch_size.value = 60;
+cfg.meal.lunch_size.range = [50, 70];
+
+cfg.meal.snack2_time.value = 14;
+cfg.meal.snack2_time.range = [13, 15];
+cfg.meal.snack2_size.value = 20;
+cfg.meal.snack2_size.range = [10, 30];
+
+cfg.meal.dinner_time.value = 18;
+cfg.meal.dinner_time.range = [17, 19];
+cfg.meal.dinner_size.value = 60;
+cfg.meal.dinner_size.range = [50, 70];
+
+cfg.meal.snack3_time.value = 20;
+cfg.meal.snack3_time.range = [19, 21];
+cfg.meal.snack3_size.value = 20;
+cfg.meal.snack3_size.range = [10, 30];
+
+
+
+
+
+
